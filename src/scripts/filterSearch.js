d0ee7e4b2497f81901164a31ed6d7830aaabbc2f
Jose Orea <jose.orea.15@gmail.com>
2019-10-18 17:34:57 -0400

    fixed webpack

--- src/scripts/filterSearch.js
+++ src/scripts/filterSearch.js
@@ -1,17 +1,15 @@
 import { grabNBAPlayer } from "./nba_util";
-import { findPlayerUrlHelper } from "./urlHelper";
+import { findPlayerUrlHelper, getStatsUrlHelper } from "./urlHelper";
 
 export const filterSearch = (playerName, data, year) => {
   let total_pages = data.meta.total_pages;
   let playerids = [];
-  debugger;
-
   if (total_pages > 1) {
     playerids = playerids.concat(Object.values(data.data).map(item => item.id));
     for (let i = 2; i <= total_pages; i++) {
       let url = findPlayerUrlHelper(playerName) + `&current_page=${i}`;
       grabNBAPlayer(url).then(data => {
-        debugger;
+        // debugger;
         console.log(Object.values(data.data).map(item => item.id));
         playerids = playerids.concat(
           Object.values(data.data).map(item => item.id)
@@ -19,8 +17,9 @@ export const filterSearch = (playerName, data, year) => {
       });
     }
   } else {
-    debugger;
+    // debugger;
     playerids = playerids.concat(Object.values(data.data).map(item => item.id));
   }
+  playerids.forEach(id => {});
   console.log(playerids);
 };
